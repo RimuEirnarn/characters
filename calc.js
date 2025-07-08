f455ab9fa80eb9777731ebfa41f9b03b600369e0
Rimu Eirnarn <Rimuru720@proton.me>
2025-07-08 16:59:10 +0700

    Fix API endpoint URL in calc.js

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-const SITE = "https://rimueirnarn.pythonanywhere.com/api/character"
+const SITE = "https://rimueirnarn.pythonanywhere.com/api/char/"
 
 // Handle subinput visibility
 const inputs = document.querySelectorAll('input[type="number"]');
